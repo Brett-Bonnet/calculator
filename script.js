68617f4cebce52035562e184c09461370a21d145
Brett Bonnet <brettbonnet4@gmail.com>
2019-01-21 14:59:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ const crntArgs = document.getElementById('arguments');
 const result = document.getElementById('result');
 const equalBtn = document.getElementById('#equals');
 
+Array.prototype.clear = function() {
+    this.splice(0, this.length);
+};
+
+
 let arguments = {
   firstArg: [],
   operator: '',
   secondArg: [],	
 }
 
-let array = [];
+let arr = [];
 
 
 
@@ -21,21 +26,25 @@ operBtn.forEach(button => button.addEventListener('click', runCalc));
 
 function runCalc(e) {
 let current = e.target.firstChild.nodeValue;
-let i = array.findIndex(el => isNaN(el) && el !== '.');
+let i = arr.findIndex(el => isNaN(el) && el !== '.');
 let j = i + 1;
 if (current === '=') {
-  let first = arguments.firstArg = parseFloat(array.slice(0, i).join(''));
-  let second = arguments.secondArg = parseFloat(array.slice(j).join(''));
-  let oper = arguments.operator = array[i];
+  let first = arguments.firstArg = parseFloat(arr.slice(0, i).join(''));
+  let second = arguments.secondArg = parseFloat(arr.slice(j).join(''));
+  let oper = arguments.operator = arr[i];
 	console.log(first);
 	console.log(second);
 	console.log(oper);
     console.log(getResult(first, second, oper))
 	 	 
-} else {
-  array.push(current);
-  console.log(array);
-		
+} else if (current === 'C'){
+    arr.clear();
+	
+	console.log(arr);
+
+  } else {
+	arr.push(current);
+    console.log(arr);  
   }
 }
 
@@ -47,23 +56,4 @@ function getResult(a, b, c) {
          : false;		 
 }
 
-
-
-
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-	return a / b;
-	
-}
 
