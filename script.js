edbe9c88403b0ce679fd981352095891534f3f8b
Brett Bonnet <brettbonnet4@gmail.com>
2019-01-22 10:08:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,16 +8,13 @@ const equalBtn = document.getElementById('#equals');
 Array.prototype.clear = function() {
     this.splice(0, this.length);
 };
-
-
 let arguments = {
   firstArg: [],
   operator: '',
   secondArg: [],	
 }
-
 let arr = [];
-
+const operList = ['+', '-', '*', '/'];
 
 
 numBtn.forEach(button => button.addEventListener('click', runCalc));
@@ -28,32 +25,52 @@ function runCalc(e) {
 let current = e.target.firstChild.nodeValue;
 let i = arr.findIndex(el => isNaN(el) && el !== '.');
 let j = i + 1;
-if (current === '=') {
-  let first = arguments.firstArg = parseFloat(arr.slice(0, i).join(''));
-  let second = arguments.secondArg = parseFloat(arr.slice(j).join(''));
-  let oper = arguments.operator = arr[i];
-	console.log(first);
-	console.log(second);
-	console.log(oper);
-    console.log(getResult(first, second, oper))
-	 	 
-} else if (current === 'C'){
-    arr.clear();
-	
-	console.log(arr);
+let first = arguments.firstArg = parseFloat(arr.slice(0, i).join(''));
+let second = arguments.secondArg = parseFloat(arr.slice(j).join(''));
+let oper = arguments.operator = arr[i];
 
+
+
+
+
+  if (current === '=') {
+  	 const answer = getResult(first, second, oper);	 
+	 console.log(answer);
+  } else if (current === 'C') {
+    arr.clear();
+	console.log(arr);
+  } else if (operList.includes(current)) {
+	  if (!isSecondOper(arr)) {
+			console.log('first operator');
+			arr.push(current);
+			console.log(arr);
+	    } else if (isSecondOper(arr)) { 
+		    const answer = getResult(first, second, oper);
+		    arr = [answer, current];
+		    console.log(answer);
+		    console.log(arr);
+		    console.log('second operator');
+	    }
+	
+	
   } else {
-	arr.push(current);
-    console.log(arr);  
+	  arr.push(current)
+	  console.log(arr);
   }
 }
 
 function getResult(a, b, c) {
-	return c === '+' ? a + b
+    return c === '+' ? a + b
 	     : c === '-' ? a - b
 		 : c === '*' ? a * b
 		 : c === '/' ? a / b
          : false;		 
+		 
+}
+
+function isSecondOper(array) {
+  let value = /\+|\-|\*|\//.test(array);
+  return value;
 }
 
 
